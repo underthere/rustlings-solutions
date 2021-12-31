3e931f9f28dcd2dbd14b0651a1215cdf94775e29
underthere <cltobecl@outlook.com>
2021-12-31 10:22:14 +0800

    solutions to error_handling

--- error_handling/errors6.rs
+++ error_handling/errors6.rs
@@ -8,7 +8,6 @@
 
 // Make these tests pass! Execute `rustlings hint errors6` for hints :)
 
-// I AM NOT DONE
 
 use std::num::ParseIntError;
 
@@ -23,7 +22,12 @@ impl ParsePosNonzeroError {
     fn from_creation(err: CreationError) -> ParsePosNonzeroError {
         ParsePosNonzeroError::Creation(err)
     }
+
     // TODO: add another error conversion function here.
+    fn form_parse_int(err: ParseIntError) -> ParsePosNonzeroError {
+        ParsePosNonzeroError::ParseInt(err)
+    }
+    
 }
 
 fn parse_pos_nonzero(s: &str)
@@ -31,9 +35,10 @@ fn parse_pos_nonzero(s: &str)
 {
     // TODO: change this to return an appropriate error instead of panicking
     // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
-    PositiveNonzeroInteger::new(x)
-        .map_err(ParsePosNonzeroError::from_creation)
+    match s.parse::<i64>() {
+        Ok(x) => PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation),
+        Err(e) => Err(ParsePosNonzeroError::form_parse_int(e))
+    }
 }
 
 // Don't change anything below this line.
